39b950c1159c54c75ee5d699bf6d42536a3f0104
eV (㋎) <ev@7pr.xyz>
2018-10-27 01:21:01 +0000

    add experimental merlin implementation of DLEQ proofs (#8)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![cfg_attr(all(feature = "alloc", not(feature = "std")), feature(alloc))]
 #![deny(missing_docs)]
 #![cfg_attr(feature = "cargo-clippy", feature(tool_lints))]
 #![cfg_attr(
@@ -15,6 +16,15 @@
 //!
 //! We have tried to align notation with that used in the paper
 //! [Privacy Pass: Bypassing Internet Challenges Anonymously](https://www.petsymposium.org/2018/files/papers/issue3/popets-2018-0026.pdf)
+
+#[cfg(all(feature = "alloc", not(feature = "std")))]
+extern crate alloc;
+
+//#[cfg(any(test, feature = "base64", feature = "std"))]
+#[cfg(any(test, feature = "std"))]
+#[macro_use]
+extern crate std;
+
 extern crate clear_on_drop;
 extern crate crypto_mac;
 extern crate curve25519_dalek;
@@ -25,23 +35,30 @@ extern crate rand;
 #[cfg(feature = "base64")]
 extern crate base64;
 
+#[cfg(feature = "merlin")]
+extern crate merlin;
+
 #[cfg(feature = "serde")]
 extern crate serde;
 
 #[cfg(test)]
 extern crate sha2;
 
-#[cfg(any(test, feature = "base64"))]
-#[macro_use]
-extern crate std;
-
 #[macro_use]
 mod macros;
 
+#[cfg(not(feature = "merlin"))]
 mod dleq;
+#[cfg(feature = "merlin")]
+mod dleq_merlin;
+
 pub mod errors;
 mod voprf;
 
+#[cfg(not(feature = "merlin"))]
 pub use self::dleq::*;
+#[cfg(feature = "merlin")]
+pub use self::dleq_merlin::*;
+
 pub use self::errors::*;
 pub use self::voprf::*;
